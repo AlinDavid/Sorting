c588fabf1436ef6c70cc8f7834668d3b1f0777a4
David Alin <70276073+AlinTobirama@users.noreply.github.com>
2021-01-06 10:16:02 +0200

    Create bubble_sort.py

---
+++ bubble_sort.py
@@ -0,0 +1,8 @@
+def bubble_sort(num_list):
+    for i in range(len(num_list) - 1, 0, - 1):
+        for j in range(i):
+            if num_list[j] > num_list[j + 1]:
+                temp = num_list[j]
+                num_list[j] = num_list[j + 1]
+                num_list[j + 1] = temp
+    return num_list
